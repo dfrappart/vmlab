c0569e4d1b6ee3447bd30ec5faf98f5fe4c1ecab
dfrappart <david@frappart.com>
2025-10-03 17:11:17 +0200

    update version constraint

--- terraformconfig/vmforwork1/00_versions.tf
+++ terraformconfig/vmforwork1/00_versions.tf
@@ -3,20 +3,16 @@
 ######################################################################
 
 terraform {
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.10.0"
 
   required_providers {
     azurerm = {
-      version = ">= 3.3.0"
-    }
-
-    azuread = {
-      version = ">= 2.30"
+      version = ">= 4.0.0"
     }
 
     azapi = {
       source  = "azure/azapi"
-      version = ">=1.1"
+      version = ">=2.0.0"
     }
 
 
